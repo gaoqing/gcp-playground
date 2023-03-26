2bedda36831bc7f86b516009928eddd0891f02e4
gaoqing <shaozhan2004@126.com>
2023-03-26 17:27:03 +0800

    make containing todo app persist items to localStorage

--- src/gcp/vm/node-server.js
+++ src/gcp/vm/node-server.js
@@ -1,6 +1,8 @@
 express = require('express');
 const app = express();
 const port = 6006;
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.send(`Hello World! <br/><br/>You may want to access this: <a href="/index.html">Todo App</a>`)
+});
 app.use(express.static('static'));
 app.listen(port, () => console.log('Example app listening on port 6006!'));
